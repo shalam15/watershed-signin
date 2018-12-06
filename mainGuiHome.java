fb17fea58b5be90649a32cd89a4bab134a2dd76d
Shalam15 <salamajayi@yaho.com>
2018-12-05 23:04:02 -0500

    i added to the gui of the DAR, i will complete it tomorow inshAllahu

--- mainGuiHome.java
+++ mainGuiHome.java
@@ -1,23 +1,30 @@
 import java.awt.Color;
-import java.awt.Component;
 import java.awt.EventQueue;
 import java.awt.Font;
-import java.awt.print.PrinterException;
-import java.text.MessageFormat;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JRadioButton;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
-import javax.swing.JTextPane;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
 import javax.swing.ScrollPaneConstants;
 import javax.swing.border.MatteBorder;
 
 public class mainGuiHome {
-	Employee employeeDefault = new Employee("Abdulsalam", "Ajayi");
+	Employee employeeDefault = new Employee();
 
 	private JFrame frame;
 	private static JTable table;
+	private JTextField txtHowellMill;
+	private JTextField textField;
+	private JTextField txtst;
+	private JTextField txtAjayiAbdulsalam;
+	private JTextField txtAa;
+	private JTextField textField_1;
+	private JTextField textField_2;
 
 	/**
 	 * Launch the application.
@@ -50,34 +57,17 @@ public class mainGuiHome {
 	private void initialize() {
 		frame = new JFrame();
 		frame.getContentPane().setBackground(Color.WHITE);
-		frame.setBounds(100, 100, 595, 842);
+		frame.setBounds(100, 100, 690, 914);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 
 		JLabel lblWatershedSigninSheet = new JLabel("Daily Activity Report");
-		lblWatershedSigninSheet.setFont(new Font("Apple Color Emoji", Font.BOLD, 17));
 		lblWatershedSigninSheet.setBounds(211, 6, 229, 16);
+		lblWatershedSigninSheet.setFont(new Font("Apple Color Emoji", Font.BOLD, 17));
 		frame.getContentPane().add(lblWatershedSigninSheet);
-
-		JTextPane textPane = new JTextPane();
-		textPane.setText(employeeDefault.getFirstName() + " " + employeeDefault.getLastName());
-		textPane.setForeground(Color.BLACK);
-		textPane.setBounds(58, 51, 145, 16);
-		frame.getContentPane().add(textPane);
-
-		JLabel lblName = new JLabel("Name:");
-		lblName.setBounds(19, 51, 41, 16);
-		frame.getContentPane().add(lblName);
-
-		JTextPane textPane_1 = new JTextPane();
+		employeeDefault.setFirstName("Abdulsalam");
+		employeeDefault.setLastName("Ajayi");
 		employeeDefault.setShiftTime("Night Shift");
-		textPane_1.setText(employeeDefault.getShiftTime());
-		textPane_1.setForeground(Color.BLACK);
-		textPane_1.setBounds(250, 51, 145, 16);
-		frame.getContentPane().add(textPane_1);
-		JLabel lblShift = new JLabel("Shift:");
-		lblShift.setBounds(211, 51, 41, 16);
-		frame.getContentPane().add(lblShift);
 
 		String[][] data = { { "0600",
 				"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum." },
@@ -95,33 +85,179 @@ public class mainGuiHome {
 		String[] columnNames = { "Time", "Chronological Comment" };
 
 		table = new JTable(data, columnNames);
-		table.setBorder(new MatteBorder(1, 1, 1, 1, Color.RED));
-		table.setBounds(59, 309, 313, -163);
-		frame.getContentPane().add(table);
+		table.setSurrendersFocusOnKeystroke(true);
+		table.setBounds(2, 18, 510, 96);
+		table.setBorder(new MatteBorder(1, 1, 1, 1, Color.blue));
 
 		JScrollPane scrollPane = new JScrollPane(table);
+		scrollPane.setBounds(186, 159, 482, 704);
 		scrollPane.setToolTipText("Testing");
 		scrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
-		scrollPane.setBounds(121, 110, 468, 704);
 		frame.getContentPane().add(scrollPane);
 
-		JScrollPane scrollPane_1 = new JScrollPane((Component) null);
-		scrollPane_1.setToolTipText("Testing");
-		scrollPane_1.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
-		scrollPane_1.setBounds(10, 108, 100, 706);
-		frame.getContentPane().add(scrollPane_1);
-		try {
-			table.print();
-		} catch (PrinterException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		MessageFormat header = new MessageFormat("Page {0,number,integer}");
-		try {
-			table.print(JTable.PrintMode.FIT_WIDTH, header, null);
-		} catch (java.awt.print.PrinterException e) {
-			System.err.format("Cannot print %s%n", e.getMessage());
-		}
+		JPanel panel = new JPanel();
+		panel.setBounds(10, 159, 164, 704);
+		frame.getContentPane().add(panel);
+
+		JLabel lblRadioCharger = new JLabel("1. Radio & Charger");
+		panel.add(lblRadioCharger);
+
+		JRadioButton rdbtnYes1 = new JRadioButton("Yes");
+		panel.add(rdbtnYes1);
+
+		JRadioButton rdbtnNo1 = new JRadioButton("No");
+		panel.add(rdbtnNo1);
+
+		JLabel lblCellPhoneReceive = new JLabel("2. Cell Phone Received");
+		panel.add(lblCellPhoneReceive);
+
+		JRadioButton rdbtnYes2 = new JRadioButton("Yes");
+		panel.add(rdbtnYes2);
+
+		JRadioButton rdbtnNo2 = new JRadioButton("No");
+		panel.add(rdbtnNo2);
+
+		JLabel lblUpdatedInstructions = new JLabel("3. Updated Instructions");
+		panel.add(lblUpdatedInstructions);
+
+		JRadioButton rdbtnYes3 = new JRadioButton("Yes");
+		panel.add(rdbtnYes3);
+
+		JRadioButton rdbtnNo3 = new JRadioButton("No");
+		panel.add(rdbtnNo3);
+		JLabel lblSecurityBranches = new JLabel("4. Security Branches");
+		panel.add(lblSecurityBranches);
+
+		JRadioButton rdbtnYes4 = new JRadioButton("Yes");
+		panel.add(rdbtnYes4);
+
+		JRadioButton rdbtnNo4 = new JRadioButton("No");
+		panel.add(rdbtnNo4);
+
+		JLabel lblSafetyHazard = new JLabel("5. Safety Hazard");
+		panel.add(lblSafetyHazard);
+
+		JRadioButton rdbtnYes5 = new JRadioButton("Yes");
+		panel.add(rdbtnYes5);
+
+		JRadioButton rdbtnNo5 = new JRadioButton("No");
+		panel.add(rdbtnNo5);
+		JLabel lblSuspiciousActivity = new JLabel("6. Suspicious Activity");
+		panel.add(lblSuspiciousActivity);
+
+		JRadioButton rdbtnYes6 = new JRadioButton("Yes");
+		panel.add(rdbtnYes6);
+
+		JRadioButton rdbtnNo6 = new JRadioButton("No");
+		panel.add(rdbtnNo6);
+		JLabel lblPolicyViolation = new JLabel("7. Policy Violation");
+		panel.add(lblPolicyViolation);
+
+		JRadioButton rdbtnYes7 = new JRadioButton("Yes");
+		panel.add(rdbtnYes7);
+
+		JRadioButton rdbtnNo7 = new JRadioButton("No");
+		panel.add(rdbtnNo7);
+		JLabel lblInjuriesIllnesss = new JLabel("8. Injuries/ Illnesss");
+		panel.add(lblInjuriesIllnesss);
+
+		JRadioButton rdbtnYes8 = new JRadioButton("Yes");
+		panel.add(rdbtnYes8);
+
+		JRadioButton rdbtnNo8 = new JRadioButton("No");
+		panel.add(rdbtnNo8);
+
+		JLabel lblIncidentReports = new JLabel("9. Incident Reports");
+		panel.add(lblIncidentReports);
+
+		JRadioButton rdbtnYes9 = new JRadioButton("Yes");
+		panel.add(rdbtnYes9);
+
+		JRadioButton rdbtnNo9 = new JRadioButton("No");
+		panel.add(rdbtnNo9);
+
+		JLabel lblOtherEquip = new JLabel("10. Other Equip.");
+		panel.add(lblOtherEquip);
+
+		JTextArea txtrHelloWorld = new JTextArea();
+		txtrHelloWorld.setText("N/A");
+		txtrHelloWorld.setTabSize(10);
+		panel.add(txtrHelloWorld);
+
+		JLabel lblSite = new JLabel("Site:");
+		lblSite.setBounds(10, 39, 32, 16);
+		frame.getContentPane().add(lblSite);
+
+		txtHowellMill = new JTextField();
+		txtHowellMill.setEditable(false);
+		txtHowellMill.setText("Howell Mill");
+		txtHowellMill.setBounds(159, 34, 130, 26);
+		frame.getContentPane().add(txtHowellMill);
+		txtHowellMill.setColumns(10);
+
+		JLabel lblDate = new JLabel("Date:");
+		lblDate.setBounds(375, 34, 38, 16);
+		frame.getContentPane().add(lblDate);
+
+		textField = new JTextField();
+		textField.setEditable(false);
+		textField.setText("3/8/2017");
+		textField.setBounds(416, 28, 77, 26);
+		frame.getContentPane().add(textField);
+		textField.setColumns(10);
+
+		JLabel lblShift = new JLabel("Shift:");
+		lblShift.setBounds(522, 34, 38, 16);
+		frame.getContentPane().add(lblShift);
+
+		txtst = new JTextField();
+		txtst.setEditable(false);
+		txtst.setText("1st");
+		txtst.setBounds(565, 28, 59, 26);
+		frame.getContentPane().add(txtst);
+		txtst.setColumns(10);
+
+		JLabel lblEmployeeOnDuty = new JLabel("Employee On Duty:");
+		lblEmployeeOnDuty.setBounds(10, 84, 130, 16);
+		frame.getContentPane().add(lblEmployeeOnDuty);
+
+		txtAjayiAbdulsalam = new JTextField();
+		txtAjayiAbdulsalam.setEditable(false);
+		txtAjayiAbdulsalam.setText("Ajayi Abdulsalam");
+		txtAjayiAbdulsalam.setBounds(159, 72, 130, 26);
+		frame.getContentPane().add(txtAjayiAbdulsalam);
+		txtAjayiAbdulsalam.setColumns(10);
+
+		txtAa = new JTextField();
+		txtAa.setEditable(false);
+		txtAa.setText("AA");
+		txtAa.setBounds(455, 79, 38, 26);
+		frame.getContentPane().add(txtAa);
+		txtAa.setColumns(10);
+
+		JLabel lblSignatureOrInitials = new JLabel("Signature");
+		lblSignatureOrInitials.setBounds(375, 84, 77, 16);
+		frame.getContentPane().add(lblSignatureOrInitials);
+
+		JLabel lblStartTime = new JLabel("Start Time:");
+		lblStartTime.setBounds(375, 131, 74, 16);
+		frame.getContentPane().add(lblStartTime);
+
+		textField_1 = new JTextField();
+		textField_1.setEditable(false);
+		textField_1.setBounds(455, 126, 32, 26);
+		frame.getContentPane().add(textField_1);
+		textField_1.setColumns(10);
+
+		JLabel lblEndTime = new JLabel("End TIme:");
+		lblEndTime.setBounds(499, 131, 74, 16);
+		frame.getContentPane().add(lblEndTime);
+
+		textField_2 = new JTextField();
+		textField_2.setEditable(false);
+		textField_2.setColumns(10);
+		textField_2.setBounds(565, 126, 32, 26);
+		frame.getContentPane().add(textField_2);
 
 	}
 }
